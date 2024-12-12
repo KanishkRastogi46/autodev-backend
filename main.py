745355442dbb3f62a67a67bdd0229b71801ec8e4
KanishkRastogi46 <113916868+KanishkRastogi46@users.noreply.github.com>
2024-12-13 02:40:58 +0530

    added sketchToCode feature but need to fine-tune prompt

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, status, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import os
+import google.generativeai as genai
+import PIL
+import io
+from dotenv import load_dotenv
 # from fastapi.security import OAuth2PasswordBearer
+load_dotenv()
 
 from app.routes.users import user_router
 from app.routes.auth import router
@@ -31,6 +37,27 @@ app.add_middleware(
     allow_methods = ["*"],
     allow_headers= ["*"],
 )
+
+genai.configure(api_key=os.getenv("GOOGLE_GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+@app.post("/upload")
+async def sketch_to_code(file: UploadFile):
+    try:
+        os.mkdir("uploads")
+        image_bytes = await file.read()
+        image = PIL.Image.open(io.BytesIO(image_bytes))
+        image.save("uploads/" + file.filename)
+        response = model.generate_content(["The image data that is provided to you will contain an image or rough sketch of a website, so you need to provide html, css and js code for that website and also keep in mind that css and js code should be inside that html tag only. Incase an image data can't be converted to html code then respond as follows:- 'Provide a proper image which contains information about website'", image])
+        print(response.text)
+        os.remove(f"uploads/{file.filename}")
+        os.rmdir("uploads")
+        return response.text
+    except:
+        return HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Error while sending image to LLM for getting code response"
+        )
 
 if __name__=="__main__":
     uvicorn.run(app)
